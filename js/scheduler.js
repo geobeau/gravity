9f4d2bf7560a16a7e9fe3c1915c09bf662e451e5
Geoffrey Beausire <g.beausire@criteo.com>
2018-02-12 21:24:45 +0100

    Attempt to handle timescale

--- js/scheduler.js
+++ js/scheduler.js
@@ -8,7 +8,7 @@ function scheduleActions(time, action){
 function runScheduler(){
   actionTmp = actionScheduler.slice(0)
   for (var i=0; i < actionTmp.length; i++) {
-    actionTmp[i]["time"] -= tickDuration;
+    actionTmp[i]["time"] -= tickDuration*rocket.timeScale;
     if (actionTmp[i]["time"] <= 0){
       executeAction(actionTmp[i]["action"]);
       actionScheduler.splice(actionScheduler.indexOf(actionTmp[i]),1);
@@ -31,7 +31,7 @@ function executeAction(action){
 function actionBurn(options){
   rocketEnginePush(options["impulsion"]/100000);
 
-  options["burnTime"] -= tickDuration;
+  options["burnTime"] -= tickDuration*rocket.timeScale;
   if(options["burnTime"] > 0){
     // We reschedule the burn for next tick
     scheduleActions(0,{"actionType":"burn","options":options})
